408f621982a624639e823ce2940ea4c5918a6fa9
Basian Lesi <lroni10094@gmail.com>
2022-04-10 23:07:18 +0200

    raspberry pi code

--- predict_model.py
+++ predict_model.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
+import h5py
+import pip
+import subprocess,sys
+# subprocess.check_call([sys.executable, "-m", "pip", "install", "numpy==1.20.1"])
+# import numpy as np
+
 from helper import *
+from keras.models import model_from_json
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -7,40 +14,56 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 print(f"ROOT DIR = {ROOT_DIR}")
 data_directory = ROOT_DIR+"/data/raw/"
 
-# df_pv = pd.read_csv(processed_data_dir + 'pv_norm.csv')
-# train_model(df_pv, model_name="pv_model",   num_of_epochs = 20, pred_col_name="PV power")
+#df_pv = pd.read_csv(processed_data_dir + 'pv_norm.csv')
+#train_model(df_pv, model_name="pv_model",   num_of_epochs = 5, pred_col_name="PV power")
 
-try:
-    df_pv = pd.read_csv(processed_data_dir + 'pv_norm.csv')
-    pv_model = load_model(model_dir + "pv_model/")
-    # pv_forecast = pd.read_csv(processed_data_dir + "PV_predict_data.csv")
-    norm = pd.read_csv(processed_data_dir + "norm.csv")
-except:
-    print("unable to load photovoltaic df and model")
-    sys.exit(1)
+# import json
 
-predict_pv_power(norm, pv_model, look_back=24, pred_col_name="PV power")
+# from keras.models import model_from_json
 
-pred_col_name="PV power"
-past = pd.read_csv(processed_data_dir + "preprocessed.csv")
-predictions = pd.read_csv("data/predictions/predicted.csv")
-y1 = past[pred_col_name]
-y2 = predictions[pred_col_name]
-x1 = past["Time"].str[0:2] +"-"+ past["Time"].str[-5:-3]
-x2 = predictions["Time"].str[0:2] +"-"+ predictions["Time"].str[-5:-3]
-plt.plot(x1, y1, label = "past")
-plt.plot(x2, y2, label = "predictions")
-x = x1.append(x2)
-xposition = get_days_change_location(x)
-for xc in xposition:
-    plt.axvline(x=xc, color='k', linestyle='--')
-x_label = []
-for t in x:
-    x_label.append(t[3:5])
-plt.xticks(ticks=x[0::3], labels=x_label[0::3])
-plt.xlabel('Time - Day-Hour')
-plt.ylabel('PV Power')
-plt.title('PV power predictions')
-plt.legend()
-plt.savefig(figures_dir + pred_col_name + 'prediction_30_March.png', format='png')
-plt.show()
+# with open(model_architecture, 'r') as json_file:
+#     architecture = json.load(json_file)
+#     pv_model = model_from_json(architecture)
+
+df_pv = pd.read_csv(processed_data_dir + 'pv_norm.csv')
+#pv_model = load_model(model_dir + "pv_model/")
+# pv_forecast = pd.read_csv(processed_data_dir + "PV_predict_data.csv")
+norm = pd.read_csv(processed_data_dir + "norm.csv")
+# print("unable to load photovoltaic df and model")
+# sys.exit(1)
+
+# subprocess.check_call([sys.executable, "-m", "pip", "install", "numpy==1.19.2"])
+# import numpy as np
+
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+
+predict_pv_power(norm, loaded_model, look_back=24, pred_col_name="PV power")
+
+# pred_col_name="PV power"
+# past = pd.read_csv(processed_data_dir + "preprocessed.csv")
+# predictions = pd.read_csv("data/predictions/predicted.csv")
+# y1 = past[pred_col_name]
+# y2 = predictions[pred_col_name]
+# x1 = past["Time"].str[0:2] +"-"+ past["Time"].str[-5:-3]
+# x2 = predictions["Time"].str[0:2] +"-"+ predictions["Time"].str[-5:-3]
+# plt.plot(x1, y1, label = "past")
+# plt.plot(x2, y2, label = "predictions")
+# x = x1.append(x2)
+# xposition = get_days_change_location(x)
+# for xc in xposition:
+#     plt.axvline(x=xc, color='k', linestyle='--')
+# x_label = []
+# for t in x:
+#     x_label.append(t[3:5])
+# plt.xticks(ticks=x[0::3], labels=x_label[0::3])
+# plt.xlabel('Time - Day-Hour')
+# plt.ylabel('PV Power')
+# plt.title('PV power predictions')
+# plt.legend()
+# plt.savefig(figures_dir + pred_col_name + 'prediction_30_March.png', format='png')
+# plt.show()
---
+++ system_requirements.md
@@ -0,0 +1,39 @@
+## Install tensorflow on raspberry pi
+
+raspberry os: https://downloads.raspberrypi.org/raspios_lite_armhf/images/ \
+python 3.7.3
+
+#### install prerequisites
+```python
+pip install cython
+sudo apt-get install gcc python3.7-dev
+pip install pycocotools==2.0.0
+pip install --upgrade numpy==1.20.1
+```
+##### in this file:
+sudo nano /home/pi/tensorflow/lib/python3.7/site-packages/tensorflow/python/ops/array_ops.py
+
+https://github.com/tensorflow/models/issues/9706#:~:text=edited-,Hi%20%40aniketbote%20I%20posted%20this%20answer%20in%20Stack%20Overflow%3A%20https,from%20tensorflow.math%20import%20reduce_prod,-%F0%9F%91%8D
+
+##### we change the following:
+```python
+from tensorflow.math import reduce_prod\
+def _constant_if_small(value, shape, dtype, name):\
+  try:\
+    if np.prod(shape) < 1000: ## --> it to if reduce_prod(shape) < 1000:\
+      return constant(value, shape=shape, dtype=dtype, name=name)\
+  except TypeError:\
+    # Happens when shape is a Tensor, list with Tensor elements, etc.\
+    pass\
+  return None
+```
+
+##### reinstall h5py
+```python
+sudo apt-get update\
+pip uninstall h5py\
+pip list\
+sudo apt-get install libhdf5-dev\
+pip install h5py\
+pip install --upgrade h5py==2.10.0
+```

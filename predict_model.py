c5ad943aa94853cc25204d3c8828882ed17957b1
Basian Lesi <lroni10094@gmail.com>
2022-05-31 18:52:54 +0200

    predict.py change load model

--- predict_model.py
+++ predict_model.py
@@ -9,21 +9,47 @@ from keras.models import model_from_json
 from upload_to_cloud import *
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+def load_model_from_json(model_name:str="model"):   # load model from json file
+    json_file = open(model_dir + model_name + '.json', 'r') # open json file
+    loaded_model_json = json_file.read()             # read json file
+    json_file.close()                           # close json file
+    log(f"Loaded model from {model_dir + model_name + '.json'}")    # log model name
+    # load json and create model
+    model = model_from_json(loaded_model_json)  # load json file
+    # load weights into model
+    model.load_weights(model_dir + model_name + ".h5")
+    log(f"Loaded weights from {model_dir + model_name + '.h5'}")
+    return model
+    
 
 def forecast_PV_power():
     print(f"ROOT DIR = {ROOT_DIR}")
     data_directory = ROOT_DIR+"/data/raw/"
 
-    df_predict = pd.read_csv(processed_data_dir + "make_predictions.csv")
+    try:
+        df_predict = pd.read_csv(processed_data_dir + "make_predictions.csv")
+        log("norm loaded")
+    except:
+        log("Unable to load data")
+        sys.exit(1)
 
-    json_file = open('model.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-    # load weights into new model
-    loaded_model.load_weights("model.h5")
+    try:
+        pv_model = load_model_from_json("pv_model")
+        log("models loaded")
+    except:
+        log("unable to load models")
+        sys.exit(1)
 
-    predict_pv_power(df_predict, loaded_model, look_back=24, pred_col_name="PV power")
+    # json_file = open('model.json', 'r')
+    # loaded_model_json = json_file.read()
+    # json_file.close()
+    # loaded_model = model_from_json(loaded_model_json)
+    # # load weights into new model
+    # loaded_model.load_weights("model.h5")
+
+    predict_pv_power(df_predict, pv_model, look_back=24, pred_col_name="PV power")
     
     
 forecast_PV_power()   
